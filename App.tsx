8a86125b504249d271d744805089899bbe86bb3b
jwandroid <cjd9408er@naver.com>
2022-06-14 03:06:28 +0900

    eslint

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { Text } from 'react-native';
 
 const App = () => {
-  return <Text>hello chat-mo</Text>;
+  return <Text>hello chat-mo!!</Text>;
 };
 
 export default memo(App);
