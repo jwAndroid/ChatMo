3963c9d543ae6cdd3df8e14e0015436e41d5dddd
jwandroid <cjd9408er@naver.com>
2022-06-16 22:24:09 +0900

    add : icon , logo , palette. change : app icon

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -9,11 +9,29 @@ declare module '*.jpeg';
 declare module '@emotion/react' {
   export interface Theme {
     font: {
-      // YoonGothicBold: string;
+      YoonGothicRegular: string;
+      YoonGothicBold: string;
     };
     color: {
       black: string;
       white: string;
+      gray_20: string;
+      gray_40: string;
+      gray_50: string;
+      gray_100: string;
+      gray_200: string;
+      gray_300: string;
+      gray_400: string;
+      gray_450: string;
+      gray_500: string;
+      gray_600: string;
+      gray_700: string;
+      gray_750: string;
+      gray_800: string;
+      gray_850: string;
+      gray_900: string;
+      gray_950: string;
+      gray_1000: string;
       tab: {
         background: string;
         text: string;
--- src/theme.ts
+++ src/theme.ts
@@ -1,20 +1,42 @@
 import { Theme } from '@emotion/react';
 
 export const font = {
-  //   YoonGothicBold: require('../assets/font/YoonGothicBold.otf'),
+  YoonGothicRegular: require('../assets/font/YoonGothicRegular.otf'),
+  YoonGothicBold: require('../assets/font/YoonGothicBold.otf'),
 };
 
 export const icon = {
-  splashicon: require('../assets/icons/splashicon.png'),
+  splash_white: require('../assets/icons/splash_white.png'),
+  splash_black: require('../assets/icons/splash_black.png'),
+  logo_white: require('../assets/icons/logo_white.png'),
+  logo_black: require('../assets/icons/logo_black.png'),
 };
 
 export const lightTheme: Theme = {
   font: {
-    //  YoonGothicBold: 'YoonGothicBold',
+    YoonGothicRegular: 'YoonGothicRegular',
+    YoonGothicBold: 'YoonGothicBold',
   },
   color: {
     black: '#000',
     white: '#fff',
+    gray_20: '#F9F9F9',
+    gray_40: '#F2F3F5',
+    gray_50: '#EBEDF0',
+    gray_100: '#E1E3E6',
+    gray_200: '#C4C8CC',
+    gray_300: '#AAAEB3',
+    gray_400: '#909499',
+    gray_450: '#7F8285',
+    gray_500: '#76787A',
+    gray_600: '#5D5F61',
+    gray_700: '#454647',
+    gray_750: '#363738',
+    gray_800: '#2C2D2E',
+    gray_850: '#232324',
+    gray_900: '#19191A',
+    gray_950: '#141414',
+    gray_1000: '#0A0A0A',
     tab: {
       background: '#fff',
       text: '#000',
@@ -26,11 +48,29 @@ export const lightTheme: Theme = {
 
 export const darkTheme: Theme = {
   font: {
-    //  YoonGothicBold: 'YoonGothicBold',
+    YoonGothicRegular: 'YoonGothicRegular',
+    YoonGothicBold: 'YoonGothicBold',
   },
   color: {
-    black: '#000',
-    white: '#fff',
+    black: '#000000',
+    white: '#ffffff',
+    gray_20: '#F9F9F9',
+    gray_40: '#F2F3F5',
+    gray_50: '#EBEDF0',
+    gray_100: '#E1E3E6',
+    gray_200: '#C4C8CC',
+    gray_300: '#AAAEB3',
+    gray_400: '#909499',
+    gray_450: '#7F8285',
+    gray_500: '#76787A',
+    gray_600: '#5D5F61',
+    gray_700: '#454647',
+    gray_750: '#363738',
+    gray_800: '#2C2D2E',
+    gray_850: '#232324',
+    gray_900: '#19191A',
+    gray_950: '#141414',
+    gray_1000: '#0A0A0A',
     tab: {
       background: 'gray',
       text: '#fff',
