41063c5a79ff0e7619fb909c3a227f61cecdfccb
jwandroid <cjd9408er@naver.com>
2022-06-19 02:32:35 +0900

    update StackHeader shadow

--- src/components/common/StackHeader.tsx
+++ src/components/common/StackHeader.tsx
@@ -1,4 +1,5 @@
 import { FC, memo, ReactNode } from 'react';
+import { Platform } from 'react-native';
 import styled from '@emotion/native';
 
 import StyledText from './StyledText';
@@ -10,8 +11,33 @@ const HeaderContainer = styled.View(({ theme }) => ({
   alignItems: 'center',
   justifyContent: 'space-between',
   paddingHorizontal: 20,
+  marginTop: Platform.OS === 'android' ? -2.5 : 0,
   backgroundColor: theme.color.header.background,
 }));
+
+const ShadowContainer = styled.View(({ theme }) => {
+  const shadow = Platform.select({
+    ios: {
+      shadowColor: '#000000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 1,
+    },
+    android: {
+      elevation: 3,
+    },
+  });
+
+  return {
+    ...shadow,
+    zIndex: 1,
+    width: '100%',
+    backgroundColor: theme.color.background,
+  };
+});
 
 interface IHeader {
   title: ReactNode;
@@ -19,9 +45,11 @@ interface IHeader {
 
 const StackHeader: FC<IHeader> = ({ title }) => {
   return (
-    <HeaderContainer>
-      <StyledText isBlod>{title}</StyledText>
-    </HeaderContainer>
+    <ShadowContainer>
+      <HeaderContainer>
+        <StyledText isBlod>{title}</StyledText>
+      </HeaderContainer>
+    </ShadowContainer>
   );
 };
 
