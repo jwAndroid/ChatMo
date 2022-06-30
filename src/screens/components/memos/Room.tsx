c0b866e711f4233dee3de55cca31f8da4d365a7b
jwandroid <cjd9408er@naver.com>
2022-06-30 22:01:35 +0900

    Cleanup: Room

--- src/screens/components/memos/Room.tsx
+++ src/screens/components/memos/Room.tsx
@@ -1,14 +1,12 @@
-import { memo, useEffect, useMemo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { useTheme } from '@emotion/react';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 import {
   MemoScreenNavigationProp,
   MemoScreenRouteProp,
 } from '../../stacks/MemoStack';
-import { ScreenContainer } from '../../../components/layout';
 import { IconHeader } from '../../../components/common/header';
 import { StyledText } from '../../../components/common/text';
 import { RoomEntity } from '../../../model';
@@ -21,8 +19,6 @@ const Room = () => {
   const navigation = useNavigation<MemoScreenNavigationProp>();
 
   const [post, setPost] = useState<RoomEntity>();
-
-  const insets = useMemo(() => -(getStatusBarHeight() * 2), []);
 
   useEffect(() => {
     setPost(params);
@@ -38,14 +34,13 @@ const Room = () => {
             : `${post?.title.substring(0, 15)}...`
         }
         backIcon
-        marginBottom={insets}
         one={theme.icon.more}
         onOnePress={() => console.log('more')}
       />
 
-      <ScreenContainer>
-        <StyledText>{ellipsize('sadfklasdfljaslkdfjlkasdjflkj', 5)}</StyledText>
-      </ScreenContainer>
+      <StyledText color="white">
+        {ellipsize('sadfklasdfljaslkdfjlkasdjflkj', 5)}
+      </StyledText>
     </View>
   );
 };
